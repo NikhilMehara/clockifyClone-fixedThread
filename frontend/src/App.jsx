905aaf705d0df2d779bb935543b2c0df024bad44
J-Prajwal <luckyjais058@gmail.com>
2022-08-24 22:57:13 +0530

    Homepage complete and chat box added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Chat } from "./Components/Chat";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import MainRoutes from "./Pages/MainRoutes";
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <MainRoutes />
       <Footer />
+      <Chat />
     </div>
   );
 }
