f90e43f13491327cf459cc84a77e37fbdaddc78b
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-08-25 07:45:01 +0530

    issue final

--- frontend/src/Pages/Downloads.jsx
+++ frontend/src/Pages/Downloads.jsx
@@ -20,7 +20,7 @@ const Downloads = () => {
       <Headers />
 
       <Box bg={"#f7fcff"} p="4rem">
-        <Text fontSize={"3xl"} textAlign="center" pt="2rem">
+        <Text fontSize={["md","lg"]} textAlign="center" pt="2rem">
           Start tracking time with Clockify
         </Text>
         <VStack>
