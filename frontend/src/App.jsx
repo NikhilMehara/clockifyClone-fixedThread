36b98f0fdda884c2707e2c899f3178fbb6d3b0ba
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-08-25 07:38:20 +0530

    color issuse resolve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
+import Downloads from './Pages/Downloads';
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Downloads/>
+  </div>;
 }
 
 export default App;
--- frontend/src/Pages/Downloads.jsx
+++ frontend/src/Pages/Downloads.jsx
@@ -23,26 +23,41 @@ const Downloads = () => {
         <Text fontSize={"3xl"} textAlign="center" pt="2rem">
           Start tracking time with Clockify
         </Text>
-        <Box display="flex" justifyContent="center" pt="2rem" pb="0.2rem">
+        <VStack>
           <Button
-            bg={"#0277bd"}
-            border={"none"}
-            color="#fff"
-            _hover={{ bg: "#0397bd" }}
-            pt="6"
-            pb="6"
-            pl="14"
-            pr="14"
+            color={"white"}
+            p={[1, 7]}
+            boxShadow={"md"}
+            _hover={{ bg: "blue.400" }}
+            bgColor={"#03A9F4"}
+            borderBottom={"4px solid #008CCF"}
+            w={"280px"}
+            h={"56px"}
           >
-            CREATE FREE ACCOUNT
+            <Link to="/">
+              <Text fontWeight={400} fontSize={["xs", "md"]}>
+                CREATE FREE ACCOUNT
+              </Text>
+            </Link>
           </Button>
-        </Box>
-        <HStack justifyContent="center" pb="2rem">
-          <FaUsers />
-          <Text _hover={{ textDecoration: "underline" }} cursor="pointer">
-            136,180 people signed up last month
-          </Text>
-        </HStack>
+          <Button
+            p={0}
+            colorScheme={"none"}
+            color={"blue.400"}
+            _hover={{ textDecoration: "underline" }}
+          >
+            <HStack>
+              <Image
+                src={"https://clockify.me/assets/images/signed-up-icon.svg"}
+              ></Image>
+              <Text fontSize={"xs"} ml={"1"}>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  134,602 people signed up last month
+                </a>
+              </Text>
+            </HStack>
+          </Button>
+        </VStack>
       </Box>
     </Box>
   );
@@ -60,7 +75,9 @@ const Headers = () => {
         mt="5rem"
         gap={"2"}
       >
-        <Text>Time tracking apps</Text>
+        <Text pt="3" pb="3">
+          Time tracking apps
+        </Text>
       </Box>
       <Text>
         Install Clockify and track time from anywhere — everything is synced
@@ -111,15 +128,39 @@ const DeviceDiff = () => {
 };
 
 const textWrite = [
-  { name: "Idle detection",span:"Stop Timer from Tracking time you speed away fron your computer" },
-  { name: " Auto start/stop",span:"Automatically start or stop the timer when you turn on your computer or browser." },
-  { name: " Offline mode" ,span:"Get notification when you forgatto start a timer."},
-  { name: " Default project",span:"Stop Timer from Tracking time you speed away fron your computer" },
-  { name: "Reminders",span:"Get notification when you forgatto start a timer." },
+  {
+    name: "Idle detection",
+    span: "Stop Timer from Tracking time you speed away fron your computer",
+  },
+  {
+    name: " Auto start/stop",
+    span: "Automatically start or stop the timer when you turn on your computer or browser.",
+  },
+  {
+    name: " Offline mode",
+    span: "Get notification when you forgatto start a timer.",
+  },
+  {
+    name: " Default project",
+    span: "Stop Timer from Tracking time you speed away fron your computer",
+  },
+  {
+    name: "Reminders",
+    span: "Get notification when you forgatto start a timer.",
+  },
   { name: "Pomodoro timer" },
-  { name: "Auto tracker", span:"Get notification when you forgatto start a timer."},
-  { name: "Expenses",span:"Stop Timer from Tracking time you speed away fron your computer" },
-  { name: "Time off" ,span:"Get notification when you forgatto start a timer."},
+  {
+    name: "Auto tracker",
+    span: "Get notification when you forgatto start a timer.",
+  },
+  {
+    name: "Expenses",
+    span: "Stop Timer from Tracking time you speed away fron your computer",
+  },
+  {
+    name: "Time off",
+    span: "Get notification when you forgatto start a timer.",
+  },
 ];
 
 const BadgeWrapper = ({ data }) => {
@@ -127,14 +168,21 @@ const BadgeWrapper = ({ data }) => {
     <>
       {data.map((el, index) => (
         <Tooltip
-          hasArrow
           key={index}
           label={el.span}
+          hasArrow
           bg="black"
           color="#fff"
-          placement="top-start"
+          placement="top"
         >
-          <Badge colorScheme="green" ml={"2"}>
+          <Badge
+            colorScheme="green"
+            p="1"
+            _hover={{ bg: "#03a9f4", color: "#fff" }}
+            ml={"3"}
+            mt="2"
+          >
+            {" "}
             {el.name}
           </Badge>
         </Tooltip>
