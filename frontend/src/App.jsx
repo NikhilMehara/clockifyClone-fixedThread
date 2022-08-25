b0627df6782ce88be72c9765abe7c9d150d34e9f
Arfeen Mushtaque <arfeenmushtaque1998@gmail.com>
2022-08-25 12:56:13 +0530

    2nd-day

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import MainRoutes from "./Pages/MainRoutes";
-import Signup from "./Pages/Signup";
+
 
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
-      {/* <MainRoutes /> */}
-      <Signup/>
-      {/* <Footer /> */}
+      <Navbar />
+      <MainRoutes />
+      <Footer />
     </div>
   );
 }
